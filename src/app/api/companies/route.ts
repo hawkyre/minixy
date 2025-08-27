84312d8070e8d840b223c7d3a7d9fd78c0c96f03
hawkyre <hawkyre@gmail.com>
2025-08-27 19:29:23 +0200

    basic GET companies api route

---
+++ src/app/api/companies/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { companiesTable } from '@/db/schema';
+
+export async function GET() {
+  try {
+    const db = drizzle(process.env.DATABASE_URL!);
+    const companies = await db.select().from(companiesTable);
+
+    return NextResponse.json(
+      {
+        companies: companies,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error('Error retrieving companies:', error);
+    return NextResponse.json(
+      { error: 'Failed to retrieve companies' },
+      { status: 500 }
+    );
+  }
+}
